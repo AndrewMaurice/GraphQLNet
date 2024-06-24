916ab2a19cb5ccd75133b75e9496ec3724054ab2
Andrew Maurice <andrewmaurice123@gmail.com>
2024-06-24 17:54:35 +0300

    updating project structure

--- Database/ApplicationDbContext.cs
+++
@@ -1,8 +0,0 @@
-using Microsoft.EntityFrameworkCore;
-
-namespace Database;
-
-public class ApplicationDbContext : DbContext
-{
-    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) {}
-}
